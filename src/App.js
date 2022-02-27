98181577e7f73619aaabb3847ae99e8c42aa717a
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-27 18:52:31 +0700

    small change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ContactApps from "./views/ContactApps";
+import CategoryContact from "./views/ContactApps";
+import DetailContact from "./views/ContactApps";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={ContactApps} />
+        <Route path="/category" element={CategoryContact} />
+        <Route path="/detail" element={DetailContact} />
       </Routes>
     </>
   );
