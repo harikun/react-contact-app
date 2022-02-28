5bb727ba6c36b018a686591cb7d923da780de56a
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-28 20:23:15 +0700

    import navbar to app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import ContactApps from "./views/ContactApps";
 import CategoryContact from "./views/ContactApps";
 import DetailContact from "./views/ContactApps";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={ContactApps} />
         <Route path="/category" element={CategoryContact} />
