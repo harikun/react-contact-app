930acb237ed540a471baf81fe3ec44952677522a
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-01 17:38:25 +0700

    add redux in categorycontact

--- src/views/CategoryContact.jsx
+++ src/views/CategoryContact.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function CategoryContact() {
-  return <p>CategoryContact</p>;
+  const dispatch = useDispatch();
+
+  const loading = useSelector((state) => state.loadingReducer.loading);
+
+  useEffect(() => {
+    dispatch({ type: "LOADING", payload: true });
+    setTimeout(() => {
+      dispatch({ type: "LOADING", payload: false });
+    }, 2000);
+  }, [dispatch]);
+  return (
+    <>
+      loading ? <p>loading...</p> :<p>CategoryContact</p>;
+    </>
+  );
 }
 
 export default CategoryContact;
