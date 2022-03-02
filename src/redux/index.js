093062ca1b1333b35481d9c4be384c463cdb32d0
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-02 13:53:24 +0700

    import concactReducer to redux store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import loadingReducer from "./reducers/loading";
+import contactReducer from "./reducers/contact";
 
 const reducers = combineReducers({
   loadingReducer,
+  contactReducer,
 });
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
