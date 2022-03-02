8102e4faa1fee01d443a9735c8c96ecb4f7c87dc
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-02 14:18:00 +0700

    create getAllContact action

---
+++ src/redux/action/getAllContact.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const allContact = () => {
+  return function (dispatch) {
+    dispatch({ type: "LOADING", payload: true });
+    axios({
+      url: "http://localhost:3001/contacts",
+      method: "get",
+    })
+      .then(({ data }) => {
+        dispatch({ type: "LOADING", payload: false });
+        dispatch({ type: "ALL_CONTACR", payload: data });
+      })
+      .catch((err) => {
+        dispatch({ type: "LOADING", payload: false });
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          title: `${err.message} ${err.status}`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  };
+};
