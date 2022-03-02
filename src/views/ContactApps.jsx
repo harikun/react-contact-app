f12608b6d0e4010cf778b1108c1a87c51f5f8202
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-02 14:41:08 +0700

    showdata in ContactApps.jsx

--- src/views/ContactApps.jsx
+++ src/views/ContactApps.jsx
@@ -1,5 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import getAllContact from "../redux/actions/getAllContact";
+
 function ContactApps() {
-  return <p>Homepage</p>;
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.loadingReducer.loading);
+  const contacts = useSelector((state) => state.contactReducer.allContact);
+
+  useEffect(() => {
+    dispatch(getAllContact());
+  }, []);
+  return (
+    <>
+      <div className="container mt-2">
+        <div className="row">
+          {loading ? (
+            <div className="d-flex justify-content-center mt-5">
+              <div className="spinner-border" role="status">
+                <span className="visually-hiddeen">Loading...</span>
+              </div>
+            </div>
+          ) : contacts.length === 0 ? (
+            <div className="text-center m-2 p-4">Data Not Found</div>
+          ) : (
+            contacts.map((contact) => {
+              return (
+                <div className="col-md-4 p-1" key={contact.id}>
+                  <div className="card shadow radius">
+                    <div className="card-body">
+                      <h5 className="card-title">{contact.name}</h5>
+                      <p className="card-text">{contact.email}</p>
+                      <p className="card-text">{contact.phonenumber}</p>
+                      <p className="card-text">category: {contact.category}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ContactApps;
